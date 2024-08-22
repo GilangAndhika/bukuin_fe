11001158d59ad364a193aa4926811de6d6c49bdf
gryzlegrizz <gryzlegrizz@gmail.com>
2024-08-22 20:33:36 +0700

    chore: Update book card layout and cover image in getbook.js

--- assets/js/getbook.js
+++ assets/js/getbook.js
@@ -47,18 +47,30 @@ function updateCards(books) {
         // Check if the cover image URL is valid
         const coverImageUrl = book.cover_image_url && book.cover_image_url.startsWith('https://')
             ? book.cover_image_url
-            : 'https://i.pinimg.com/originals/6b/e3/88/6be3885c283e4ea74505fd3b9ffd8f78.jpg';
+            : 'https://i.pinimg.com/originals/49/e5/8d/49e58d5922019b8ec4642a2e2b9291c2.png';
 
         const card = document.createElement('div');
         card.className = 'bg-white shadow-md rounded-lg overflow-hidden';
         card.innerHTML = `
-            <div class="bg-cover bg-center h-48" style="background-image: url('${coverImageUrl}');"></div>
-            <div class="p-4">
-                <h2 class="text-lg font-semibold">${book.title}</h2>
-                <p class="text-sm text-gray-600">${book.author}</p>
-                <p class="text-sm text-gray-500 mt-2">${book.description}</p>
-                <p class="text-sm text-gray-500 mt-2">Launch Year: ${book.launch_year}</p>
-                <p class="text-sm text-gray-500 mt-2">ISBN: ${book.isbn}</p>
+            <div class="bg-cover bg-center" style="background-image: url('${coverImageUrl}');">
+                <div class="bg-gradient-to-t from-transparent via-transparent to-black ">
+                    <div class="bg-gradient-to-b from-transparent via-transparent to-black h-96">
+                        <div class="p-4 grid grid-rows-3 grid-flow-row justify-between h-full">
+                            <div class="row-span-1">
+                                <h2 class="text-2xl font-semibold font-[('Montserrat')] text-blue-100">${book.title}</h2>
+                                <p class="text-sm text-gray-400 italic">${book.author}</p>
+                            </div>
+                            
+                            <div class="row-span-2 flex flex-col justify-end gap-2 text-sm text-gray-200">
+                                <p class="text-blue-100">${book.description}</p>
+                                <div class="flex gap-4 text-xs text-blue-200 font-thin">
+                                    <p>Launch Year: ${book.launch_year}</p>
+                                    <p>ISBN: ${book.isbn}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>                    
+                </div>
             </div>
         `;
         container.appendChild(card);
